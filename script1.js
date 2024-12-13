7d68f6bdaa7477aa416813050bb97e0c52ad3694
tuyn1317 <nguyenhuutuyen19082003@gmail.com>
2024-12-13 21:52:07 +0700

    poject

--- script1.js
+++ script1.js
@@ -3,7 +3,7 @@ const messages = [
 ];
 
 function generateRandomNotifications() {
-  const notificationCount = 100;
+  const notificationCount = 200;
 
   for (let i = 0; i < notificationCount; i++) {
     setTimeout(() => {
